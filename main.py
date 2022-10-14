c526362b25d8fee36edc130d09c1ed7f659d3a4f
Sharan Bhatia <s.s.bhatia@vikes.csuohio.edu>
2022-10-14 18:00:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ import os
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("hello")
+    print("Hello World")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
